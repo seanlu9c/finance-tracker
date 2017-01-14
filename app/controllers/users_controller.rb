fd7f68b928ec60e708b39452184f1eefca6dc3ab
Sean <seanlu9c@gmail.com>
2017-01-14 10:32:05 +0000

    add user and stock association and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def my_portfolio
     
+    ## initiate variables
+    
+    @user_stocks = current_user.stocks
+    @user = current_user
+
   end
 end
